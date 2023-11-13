6f81bb8e712bd56db1f29db3ec4514001ea0009a
Brandon Hinesley <bhinesley@gmail.com>
2023-11-13 22:58:12 +0000

    tmp log post data

--- app/app.py
+++ app/app.py
@@ -2,16 +2,14 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET', 'POST'])
-def index():
-    if request.method == 'GET':
-        return "Hello"
-    elif request.method == 'POST':
-        rows = ['='.join(item) for item in request.form.items()]
-        s_post_data = '\n'.join(rows)
-        with open("post.log", "a") as f_log:
-            f_log.writelines(s_post_data)
-        return s_post_data + '\n'
+@app.route('/write', methods = ['POST'])
+def write():
+    rows = ['='.join(item) for item in request.form.items()]
+    s_post_data = '\n'.join(rows)
+    with open("post.log", "a") as f_log:
+        f_log.write(str(len(rows)))
+        f_log.writelines(s_post_data)
+    return s_post_data + '\n'
 
 @app.route('/log')
 def log():
